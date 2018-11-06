005a69d9471e3850f6b6927c3f6b6078f3fc8148
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-11-06 21:19:48 +0100

    Now example.py accepts commands
     you can pass an angle like this: python example.py 50

--- example.py
+++ example.py
@@ -1,8 +1,16 @@
 
 from Servo import Servo
+import sys
 
 
-def main(args):
+def main(argv):
 
+    angle = int(argv[0])
     my_servo = Servo(11, "Sg90")
-    my_servo.move_angle(args)
+    my_servo.move_angle(angle)
+    print("Moving: ", angle)
+
+
+if __name__ == "__main__":
+
+    main(sys.argv[1:])
