4bbab83b8175b7a7c0b7d7087801f57e14599c31
Dídac Coll Pujals <didaccoll_93@hotmail.com>
2018-11-06 21:57:41 +0100

    cleaning up ports

--- example.py
+++ example.py
@@ -1,15 +1,17 @@
 
 from Servo import Servo
 import sys
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 def main(argv):
 
+    GPIO.setmode(GPIO.BOARD)
     angle = int(argv[0])
     my_servo = Servo(11, "Sg90")
     my_servo.move_angle(angle)
     print("Moving: ", angle)
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
